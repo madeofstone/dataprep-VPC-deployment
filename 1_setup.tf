1962da995eb3ca14a729277c3a12974a67ae0596
Travis Stone <travis.stone@alteryx.com>
2023-07-10 14:07:47 +0200

    added k8s provider to 1_setup.tf

--- 1_setup.tf
+++ 1_setup.tf
@@ -5,6 +5,11 @@ terraform {
         source = "hashicorp/google"
         version = "4.68.0"
     }
+    
+     kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
   }
     required_version = ">= 0.14"
   }
